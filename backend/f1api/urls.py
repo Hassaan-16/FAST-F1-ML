0c9c23c0f864fa3640508b364557450ab2a66d92
Hassaan <hassaanbinsaqib03@gmail.com>
2025-08-15 21:19:14 +0500

    Standings Page API linked
    Standings Page API linked.
    now go over it and fine tune the UI.

--- backend/f1api/urls.py
+++ backend/f1api/urls.py
@@ -1,3 +1,4 @@
+#urls.py 
 from django.urls import path
 from . import views
 
@@ -8,4 +9,14 @@ urlpatterns = [
     path('full-calendar/', views.full_calendar, name='full_calendar'),
     path('drivers/', views.drivers, name='drivers'),
     path('teams/', views.teams, name='teams'),
+
+    path('schedule/', views.get_event_schedule, name='event-schedule'),
+    path('schedule/<int:year>/', views.get_event_schedule, name='event-schedule-year'),
+    path('results/<int:year>/<str:event>/<str:session>/', views.get_session_results, name='session-results'),
+    
+    
+    path('standings/drivers/', views.get_standings, {'type': 'drivers'}, name='driver-standings'),
+    path('standings/drivers/<int:year>/', views.get_standings, {'type': 'drivers'}, name='driver-standings-year'),
+    path('standings/constructors/', views.get_standings, {'type': 'constructors'}, name='constructor-standings'),
+    path('standings/constructors/<int:year>/', views.get_standings, {'type': 'constructors'}, name='constructor-standings-year'),
 ]
